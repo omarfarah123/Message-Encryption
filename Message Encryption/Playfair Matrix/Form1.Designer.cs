724c30b08b9c64a24a4ee47c3d10af238f7132eb
omarfarah123 <76454677+omarfarah123@users.noreply.github.com>
2021-12-26 09:55:55 -0500

    Complete solution

---
+++ Message Encryption/Playfair Matrix/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Playfair_Matrix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSecretWord = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btn_Translate_Text = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(42, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cipher \"Secret\" Word:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtSecretWord
+            // 
+            this.txtSecretWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSecretWord.Location = new System.Drawing.Point(243, 63);
+            this.txtSecretWord.Name = "txtSecretWord";
+            this.txtSecretWord.Size = new System.Drawing.Size(434, 29);
+            this.txtSecretWord.TabIndex = 1;
+            this.txtSecretWord.TextChanged += new System.EventHandler(this.txtSecretWord_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(139, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Input Text:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(243, 133);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(434, 92);
+            this.txtInput.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(124, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Output Text:";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(244, 251);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(433, 108);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // btn_Translate_Text
+            // 
+            this.btn_Translate_Text.Location = new System.Drawing.Point(76, 390);
+            this.btn_Translate_Text.Name = "btn_Translate_Text";
+            this.btn_Translate_Text.Size = new System.Drawing.Size(121, 26);
+            this.btn_Translate_Text.TabIndex = 6;
+            this.btn_Translate_Text.Text = "Translate Text";
+            this.btn_Translate_Text.UseVisualStyleBackColor = true;
+            this.btn_Translate_Text.Click += new System.EventHandler(this.btn_Translate_Text_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(641, 390);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(121, 26);
+            this.btn_Close.TabIndex = 8;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Clear.Location = new System.Drawing.Point(226, 390);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(121, 26);
+            this.btn_Clear.TabIndex = 9;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Playfair_Matrix.Properties.Resources.encryption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_Translate_Text);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSecretWord);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSecretWord;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btn_Translate_Text;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button btn_Clear;
+    }
+}
+
---
+++ Message Encryption/Playfair Matrix/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Playfair_Matrix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSecretWord_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+       
+
+        private void btn_Translate_Text_Click(object sender, EventArgs e)
+        {
+            String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var chars = alpha.ToCharArray();
+            char[,] encrypt = new char[5, 5];
+            String secretWord = txtSecretWord.Text.ToUpper();
+            String input = txtInput.Text.ToUpper();
+            string alphaPlusSecret = secretWord + alpha;
+            alphaPlusSecret = RemoveDuplicates(alphaPlusSecret);
+            chars = alphaPlusSecret.ToCharArray();
+            char[] messageArray = input.ToCharArray();
+            string output = "";
+
+            int ctr2 = 0;
+            int row1 = 0;
+            int col1 = 0;
+
+            for (int ctr = 0; ctr < chars.Length; ctr++)
+            {
+                if (chars[ctr] == 'J')
+                    continue;
+
+                ctr2++;
+              
+                if (ctr2 == 5)
+                {
+               
+                    ctr2 = 0;
+                }
+
+               
+                if (col1 == 5)
+                {
+                    col1 = 0;
+                    row1++;
+                }
+
+                encrypt[row1, col1] = chars[ctr];
+                col1++;
+
+            }
+
+
+
+            for (int i = 0; i < messageArray.Length; i++)
+            {
+                for (int j = 0; j < encrypt.GetLength(0); j++)
+                {
+                    for (int k = 0; k < encrypt.GetLength(1); k++)
+                    {
+                        if (messageArray[i] == encrypt[j, k])
+                        {
+                            output += encrypt[k, j];
+                        }
+
+                    }
+
+                }
+                if (messageArray[i] == ' ')
+                {
+                    output += ' ';
+                }
+                else if (messageArray[i] == '.') 
+                {
+                    output += '.';
+                }
+            }
+            txtOutput.Text += output;
+            
+            
+
+        }
+      
+        static string RemoveDuplicates(string key)
+        {
+            // Remove duplicate chars using string concats.
+            // ... Store encountered letters in this string.
+
+            string result = ""; //initialize result
+
+            foreach (char value in key)
+            {
+                // See if character is in the result already; if not,
+                //then, append that character to "result"
+                if (result.IndexOf(value) == -1)
+                {
+                    result += value;
+                }
+            }
+            return result;
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = null;
+            txtOutput.Text = null;
+            txtSecretWord.Text = null;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
